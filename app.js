56cf6e237405ffbb92d1319d26d1e6536dd6b468
vainotuisk <vainotuisk@gmail.com>
2016-10-09 10:43:10 +0300

    initial setup

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+(function () {
+'use strict';
 angular.module('NarrowItDownApp',[])
-.controller("NarrowItDownController",NarrowItDownController);
+.controller("NarrowItDownController",NarrowItDownController)
+.service("MenuSearchService",MenuSearchService)
+;
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var narrowController = this;
+  narrowController.getMatchedMenuItems = function(searchTerm) {
+     MenuSearchService.getMatchedMenuItems(searchTerm);
+  }
 
- {
+  #getMatchedMenuItems(searchTerm)
+};
+
+
 
 }]);
